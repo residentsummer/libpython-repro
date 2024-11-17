5bdc82a87eeaa627babb980a961c4c4dbb50ac96
Anton S <residentsummer@gmail.com>
2024-11-17 15:29:10 +0000

    removed irrelevant code

--- core/interop.py
+++ core/interop.py
@@ -1,15 +1,3 @@
-from cljbridge.gevent_support import start_clojure_thread
-from cljbridge import clojure
-
-
-def start_clojure():
-    start_clojure_thread(['dev', 'repl'], post_init=start_app)
-
-
-def start_app():
-    clojure.require("dev")
-    clojure.resolve_call_fn("dev/-main")
-
 
 __BORROWED = {}
 
